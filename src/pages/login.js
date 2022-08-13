f230b84df818212c031b8afa5159cf2a5311d020
BholaMishra <bholam.bcom2020@ssism.org>
2022-08-13 17:25:04 +0530

    React.Js

---
+++ src/pages/login.js
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Link, Redirect} from 'react-router-dom';
+import TitleComponent from "./title";
+
+export default class Login extends Component {
+
+    state = {
+        email: '',
+        password: '',
+        redirect: false,
+        authError: false,
+        isLoading: false,
+        location: {},
+    };
+
+    handleEmailChange = event => {
+        this.setState({email: event.target.value});
+    };
+    handlePwdChange = event => {
+        this.setState({password: event.target.value});
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({isLoading: true});
+        const url = 'https://gowtham-rest-api-crud.herokuapp.com/login';
+        const email = this.state.email;
+        const password = this.state.password;
+        let bodyFormData = new FormData();
+        bodyFormData.set('email', email);
+        bodyFormData.set('password', password);
+        axios.post(url, bodyFormData)
+            .then(result => {
+                if (result.data.status) {
+                    localStorage.setItem('token', result.data.token);
+                    this.setState({redirect: true, isLoading: false});
+                    localStorage.setItem('isLoggedIn', true);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({authError: true, isLoading: false});
+            });
+    };
+
+    componentDidMount() {
+        const url = 'https://freegeoip.app/json/';
+        axios.get(url)
+            .then(response => {
+                const location = response.data;
+                this.setState({ location });
+            }) 
+            .catch(error => {
+                this.setState({ toDashboard: true });
+                console.log(error);
+            });
+    }
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to='/dashboard'/>
+        }
+    };
+
+    render() {
+        const isLoading = this.state.isLoading;
+        return (
+            <div className="container">
+                <TitleComponent title="React CRUD Login "></TitleComponent>
+                <div className="card card-login mx-auto mt-5">
+                    <div className="card-header">Login</div>
+                   
+                    <div className="card-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input className={"form-control " + (this.state.authError ? 'is-invalid' : '')} id="inputEmail" placeholder="Email address" type="text" name="email" onChange={this.handleEmailChange} autoFocus required/>
+                                    <label htmlFor="inputEmail">Email address</label>
+                                    <div className="invalid-feedback">
+                                        Please provide a valid Email.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input type="password" className={"form-control " + (this.state.authError ? 'is-invalid' : '')} id="inputPassword" placeholder="******" name="password" onChange={this.handlePwdChange} required/>
+                                    <label htmlFor="inputPassword">Password</label>
+                                    <div className="invalid-feedback">
+                                        Please provide a valid Password.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" value="remember-me"/>Remember Password
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <button className="btn btn-primary btn-block" type="submit" disabled={this.state.isLoading ? true : false}>Login &nbsp;&nbsp;&nbsp;
+                                    {isLoading ? (
+                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    ) : (
+                                        <span></span>
+                                    )}
+                                </button>
+                            </div>
+                            <div className="form-group">
+                                <div className="form-group">
+                                    <b>email:</b> mishrabhola368@gmail.com
+                                </div>
+                                <div className="form-group">
+                                    <b>password :</b> password
+                                </div>
+                            </div>
+                        </form>
+                        <div className="text-center">
+                            <Link className="d-block small mt-3" to={'register'}>Register an Account</Link>
+                            <a className="d-block small" href="forgot-password.html">Forgot Password?</a>
+                        </div>
+                    </div>
+                </div>
+                {this.renderRedirect()}
+            </div>
+        );
+    }
+}
+
+
---
+++ src/pages/register.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link, Redirect} from 'react-router-dom';
+
+export default class Register extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        redirect: false,
+        authError: false,
+        isLoading: false,
+    };
+
+    handleEmailChange = event => {
+        this.setState({ email: event.target.value });
+    };
+    handlePwdChange = event => {
+        this.setState({ password: event.target.value });
+    };
+    handleNameChange = event => {
+        this.setState({ name: event.target.value });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({isLoading: true});
+        const url = 'https://gowtham-rest-api-crud.herokuapp.com/register';
+        const email = this.state.email;
+        const password = this.state.password;
+        const name = this.state.name;
+        let bodyFormData = new FormData();
+        bodyFormData.set('email', email);
+        bodyFormData.set('name', name);
+        bodyFormData.set('password', password);
+        axios.post(url, bodyFormData)
+            .then(result => {
+                this.setState({isLoading: false});
+                if (result.data.status !== 'fail') {
+                    this.setState({redirect: true, authError: true});
+                }else {
+                    this.setState({redirect: false, authError: true});
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ authError: true, isLoading: false });
+            });
+    };
+
+    renderRedirect = () => {
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        }
+    };
+
+    render() {
+        const isLoading = this.state.isLoading;
+        return (
+            <div className="container">
+                <div className="card card-login mx-auto mt-5">
+                    <div className="card-header">Register</div>
+                    <div className="card-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input type="text" id="inputName" className="form-control" placeholder="name"  name="name" onChange={this.handleNameChange} required/>
+                                    <label htmlFor="inputName">Name</label>
+                                </div>
+                            </div>
+
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input id="inputEmail" className={"form-control " + (this.state.authError ? 'is-invalid' : '')} placeholder="Email address" type="text" name="email" onChange={this.handleEmailChange} autoFocus required/>
+                                    <label htmlFor="inputEmail">Email address</label>
+                                    <div className="invalid-feedback">
+                                        Please provide a valid Email. or Email Exis
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <div className="form-label-group">
+                                    <input type="password" className="form-control" id="inputPassword" placeholder="******"  name="password" onChange={this.handlePwdChange} required/>
+                                    <label htmlFor="inputPassword">Password</label>
+                                </div>
+                            </div>
+
+                            <div className="form-group">
+                                <button className="btn btn-primary btn-block" type="submit" disabled={this.state.isLoading ? true : false}>Register &nbsp;&nbsp;&nbsp;
+                                    {isLoading ? (
+                                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                    ) : (
+                                        <span></span>
+                                    )}
+                                </button>
+                            </div>
+                        </form>
+                        <div className="text-center">
+                            <Link className="d-block small mt-3" to={''}>Login Your Account</Link>
+                            <Link className="d-block small" to={'#'}>Forgot Password?</Link>
+                        </div>
+                    </div>
+                </div>
+                {this.renderRedirect()}
+            </div>
+        );
+    }
+}
+
+
